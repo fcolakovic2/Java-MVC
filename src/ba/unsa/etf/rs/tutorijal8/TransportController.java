2cfde0f4ac08deb118172e13f42bca3fc529f87b
fcolakovic2 <fcolakovic2@etf.unsa.ba>
2020-05-14 22:28:10 +0200

    Napravljen exit

--- src/ba/unsa/etf/rs/tutorijal8/TransportController.java
+++ src/ba/unsa/etf/rs/tutorijal8/TransportController.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rs.tutorijal8;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class TransportController {
+    public Button napustiDugme;
+    public Button potvrdaButton;
+    public TextField imeBusa;
+
+
+    public void napustiDugme(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void potvrdiIzmjene(ActionEvent actionEvent) {
+    }
 }
