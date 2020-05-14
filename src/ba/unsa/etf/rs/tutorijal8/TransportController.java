42a4ef5b7cf6349436c40eeaf3fdfc759f3e32e7
fcolakovic2 <fcolakovic2@etf.unsa.ba>
2020-05-14 20:17:41 +0200

    Implementirano exit dugme i 2 listview-a.

--- src/ba/unsa/etf/rs/tutorijal8/TransportController.java
+++ src/ba/unsa/etf/rs/tutorijal8/TransportController.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rs.tutorijal8;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
 public class TransportController {
+    public Button btnExit;
+    public ListView<Driver> listaVozaca;
+    public ListView<Bus> listaBuseva;
+
+    @FXML
+    public void initialize(){
+    }
+
+    public void exitBtn(ActionEvent actionEvent) {
+      Stage gasi=(Stage) listaBuseva.getScene().getWindow();
+      gasi.close();
+    }
+
+
+    public void dodajVozaca(ActionEvent actionEvent){
+
+    }
+
 }
